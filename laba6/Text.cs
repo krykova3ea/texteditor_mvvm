11b6698da5b022f785ca2acaf9abc69cc5b5f0ba
Helen <70480275+krykova3ea@users.noreply.github.com>
2023-05-09 23:30:32 +0300

    Add files via upload

---
+++ laba6/Text.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace laba6.Model
+{
+    public class Text: INotifyPropertyChanged
+    {
+        public string Doc { get; set; } 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
